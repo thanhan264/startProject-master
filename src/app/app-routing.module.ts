5e73bc86e86e2b67acf92389f70e336eca0bad34
thanhan264 <thanhanpro2641995@gmail.com>
2018-07-23 15:50:59 +0700

    task31

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+    { path: '', loadChildren: './layout/layout.module#LayoutModule' },
+     { path: 'access-denied', loadChildren: './access-denied/access-denied.module#AccessDeniedModule' },
+     { path: 'not-found', loadChildren: './not-found/not-found.module#NotFoundModule' },
+     { path: '**', redirectTo: 'not-found' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
